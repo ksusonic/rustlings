a29ac62d12de317eb0f609711faed94807bde7d5
Daniil <ksusonic@yandex.ru>
2024-11-02 22:00:48 +0300

    structs

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -10,13 +10,13 @@ struct Package {
 
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: u32) -> Self {
-        if weight_in_grams < 10 {
+        if weight_in_grams <= 10 {
             // This isn't how you should handle errors in Rust, but we will
             // learn about error handling later.
             panic!("Can't ship a package with weight below 10 grams");
         }
 
-        Self {
+        Package {
             sender_country,
             recipient_country,
             weight_in_grams,
@@ -24,14 +24,15 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
+    fn is_international(&self) -> bool {
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
+        self.sender_country != self.recipient_country
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
-        // TODO: Calculate the package's fees.
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
